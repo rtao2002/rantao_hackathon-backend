ac257cbcfcf33ad005de8b5384db4e5c1c6a4f22
rtao <rantaosemi@gmail.com>
2026-06-16 13:18:57 +0900

    update main.py2

--- hackathon-backend/main.py
+++ hackathon-backend/main.py
@@ -6,9 +6,18 @@ from sqlalchemy.orm import Session
 from database import engine, get_db
 from models import Base, Question, Answer
 from schemas import QuestionCreate, AnswerCreate, SimilarQuestionRequest
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
